913ac4aaee14d307f07dc1b90350308d3cea0c25
Eugene Marcotte <emarcotte@gmail.com>
2017-03-25 15:41:11 -0400

    You can click on tiles and stuff.

--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -5,15 +5,51 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    public delegate void MouseOverTile(Tile t);
+    public int X;
+    public int Y;
+    public MouseOverTile mouseOverEvent;
+    private Material material;
+
+    private bool hovered = false;
+
+    private void OnMouseOver()
+    {
+        hovered = true;
+        material.color = Color.red;
+        mouseOverEvent(this);
+    }
+
+    private void OnMouseExit()
+    {
+        hovered = false;
+        material.color = Color.white;
+    }
+
+    public void SetSelected(bool selected)
+    {
+
+    }
+
+    public void Highlight(bool on, Color c)
+    {
+        if (!hovered)
+        {
+            material.color = on ? c : Color.white;
+        }
+    }
+
     // Update is called once per frame
     internal void SetupTile(int x, int y, int w, int h)
     {
+        X = x;
+        Y = y;
         name = "Tile " + x + ", " + y;
         transform.Translate(x, y, 0);
         gameObject.SetActive(true);
         float width = w +1;
         float height = h +1;
-        var material = GetComponent<Renderer>().material;
+        material = GetComponent<Renderer>().material;
         material.mainTextureOffset = new Vector2(x / width, y / height);
         material.mainTextureScale = new Vector2(1 / width, 1 / height);
     }
--- Assets/TileGenerator.cs
+++ Assets/TileGenerator.cs
@@ -7,6 +7,8 @@ public class TileGenerator : MonoBehaviour
     public int xSize, ySize;
     public Tile basicTile;
     private Tile[] tiles;
+    private Tile over;
+    private Tile selected;
 
     void Start()
     {
@@ -14,7 +16,51 @@ public class TileGenerator : MonoBehaviour
 
     void Update()
     {
+        var click = Input.GetMouseButtonUp(0);
+        if (click)
+        {
+            if (!selected && over)
+            {
+                selected = over;
+                Debug.Log("Selected " + selected.name);
+                selected.SetSelected(true);
+            }
+            else if (selected && !over)
+            {
+                Debug.Log("Unselecting " + selected.name);
+                ChangeHighlights(false);
+                selected.SetSelected(false);
+                selected = null;
+            }
+            else if (selected && over)
+            {
+                Debug.Log("Started from " + selected.name + " going to " + over.name);
+            }
+        }
 
+        if(selected)
+        {
+            ChangeHighlights(true);
+        }
+
+        over = null;
+    }
+
+    private void ChangeHighlights(bool on)
+    {
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                var xd = selected.X - x;
+                xd = xd * xd;
+                var yd = selected.Y - y;
+                yd = yd * yd;
+                Color c = Color.green;
+                c.g *= 1 - Mathf.Clamp01(Mathf.Sqrt(xd + yd) / 10);
+                tiles[i].Highlight(on, c);
+            }
+        }
     }
 
     private void Awake()
@@ -31,7 +77,13 @@ public class TileGenerator : MonoBehaviour
             {
                 tiles[i] = Instantiate(basicTile, transform);
                 tiles[i].SetupTile(x, y, xSize, ySize);
+                tiles[i].mouseOverEvent += MouseOver;
             }
         }
+    }
+
+    private void MouseOver(Tile t)
+    {
+        this.over = t;
     }
 }
