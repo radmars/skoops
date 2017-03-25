a23f218a38ae16ef1ed533783e45a5be2a57bb8b
Eugene Marcotte <emarcotte@gmail.com>
2017-03-25 14:53:50 -0400

    I made a tile dot com

---
+++ Assets/Tile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    // Update is called once per frame
+    internal void SetupTile(int x, int y, int w, int h)
+    {
+        name = "Tile " + x + ", " + y;
+        transform.Translate(x, y, 0);
+        gameObject.SetActive(true);
+        float width = w +1;
+        float height = h +1;
+        var material = GetComponent<Renderer>().material;
+        material.mainTextureOffset = new Vector2(x / width, y / height);
+        material.mainTextureScale = new Vector2(1 / width, 1 / height);
+    }
+}
---
+++ Assets/TileGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+    public int xSize, ySize;
+    public Tile basicTile;
+    private Tile[] tiles;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void Awake()
+    {
+        Generate();
+    }
+
+    private void Generate()
+    {
+        tiles = new Tile[(xSize + 1) * (ySize + 1)];
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                tiles[i] = Instantiate(basicTile, transform);
+                tiles[i].SetupTile(x, y, xSize, ySize);
+            }
+        }
+    }
+}
